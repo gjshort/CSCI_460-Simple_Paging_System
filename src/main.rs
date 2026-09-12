9203fb6ddd769499d937c8cbbe6da282eb41303c
Griffin Short <griffin.short@student.montana.edu>
2026-09-12 15:07:05 -0600

    Handle first 2 CLI arguments. Essentially no error checking for simplicity

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 * Main File for the Paging System Simulator
 */
 
+use std::collections::VecDeque;
+use std::env;
+
 // Memory Controller Visibility
 mod memory_controller; 
 use crate::memory_controller::Job;
 use crate::memory_controller::FrameTableEntry;
-
-use std::collections::VecDeque;
 
 fn main() {
     
@@ -22,5 +23,24 @@ fn main() {
     let mut frame_table: Vec<FrameTableEntry> = Vec::new();
     let mut resident_jobs: VecDeque<u32> = VecDeque::new();
 
+    // Bring in CLI arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 4 
+    {
+        memory_size = args[1].parse::<u32>().unwrap();
+        frame_size = args[2].parse::<u32>().unwrap();
+
+        num_frames = memory_size / frame_size;
+        free_frames = num_frames;
+    
+        println!("Memory Size: {}", memory_size);
+        println!("Frame Size: {}", frame_size);
+        println!("Num Frames: {}", num_frames);
+        println!("Free Frames: {}", free_frames);
+    }
+    else 
+    {
+        println!("Invalid CLI arguments");
+    }
 
 }
